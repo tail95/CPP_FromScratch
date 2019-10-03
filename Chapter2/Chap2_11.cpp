fdd130b21ea073b61c90830c492bc9523110159b
tail95 <xodlf8637@naver.com>
2019-10-03 15:16:09 +0900

    changed ex11

--- Chapter2/Chap2_11.cpp
+++ Chapter2/Chap2_11.cpp
@@ -6,8 +6,13 @@ int main() {
 	int sum = 0;
 	cout << "끝 수를 입력하세요>>";
 	cin >> n;
-	for (int k = 1; k <= n; k++) {
+	test
+			test	e
+		test
+					teset
+			testt
+	/*for (int k = 1; k <= n; k++) {
 		sum += k;
-	}
+	}*/
 	cout << "1에서 " << n << "까지의 합은 " << sum << "입니다." << endl;
 }
