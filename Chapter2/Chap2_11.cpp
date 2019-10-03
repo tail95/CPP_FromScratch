4eed579eddcc2cf375f2a84f8a152c72cfb8f4b9
tail95 <xodlf8637@naver.com>
2019-10-03 15:17:20 +0900

    ex11

--- Chapter2/Chap2_11.cpp
+++ Chapter2/Chap2_11.cpp
@@ -2,17 +2,11 @@
 using namespace std;
 
 int main() {
-	int n = 0;
-	int sum = 0;
+	int n = sum = 0;
 	cout << "끝 수를 입력하세요>>";
 	cin >> n;
-	test
-			test	e
-		test
-					teset
-			testt
-	/*for (int k = 1; k <= n; k++) {
+	for (int k = 1; k <= n; k++) {
 		sum += k;
-	}*/
+	}
 	cout << "1에서 " << n << "까지의 합은 " << sum << "입니다." << endl;
 }
