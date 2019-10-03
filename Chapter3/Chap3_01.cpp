75db97fca41131fd4de591356d91867d3ee241a0
tail95 <xodlf8637@naver.com>
2019-10-03 21:39:05 +0900

    ex1

---
+++ Chapter3/Chap3_01.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Tower {
+	int height;
+public:
+	Tower();
+	Tower(int h);
+	int getHeight();
+};
+
+Tower::Tower() {
+	this->height = 1;
+}
+Tower::Tower(int h) {
+	this->height = h;
+}
+int Tower::getHeight() {
+	return this->height;
+}
+int main()
+{
+	Tower myTower;
+	Tower seoulTower(100);
+	cout << "높이는 " << myTower.getHeight() << "미터" << endl;
+	cout << "높이는 " << seoulTower.getHeight() << "미터" << endl;
+}
+
