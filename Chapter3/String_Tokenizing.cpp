27ecb8feada473dce9dd87fe0e5947b34e2e237c
tail95 <xodlf8637@naver.com>
2019-10-04 12:27:40 +0900

    ***** String Tokenize *****

--- Chapter3/String_Tokenizing.cpp
+++ Chapter3/String_Tokenizing.cpp
@@ -2,10 +2,10 @@
 #include <cstring>
 
 using namespace std;
-string str_arr[1000];
-int str_cnt;
 
 int main() {
+	string str_arr[1000];
+	int str_cnt = 0;
 	string a = "1945/8/15";
 	char* str_buff = new char[1000];
 	strcpy(str_buff, a.c_str());
@@ -19,5 +19,4 @@ int main() {
 	for (int n = 0; n < str_cnt; n++) {
 		cout << str_arr[n] << endl;
 	}
-	return 0;
 }
